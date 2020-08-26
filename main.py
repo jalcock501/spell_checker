b1389a56bf6972f32d115a299f16c1388dd02a44
jalcock501 <jalcock501@googlemail.com>
2020-08-26 22:06:02 +0100

    Exception handling and sys.argv added for I/O

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ PROG_VERS = '0.1'  # none production version
 # GLOBALS
 _basedir = os.path.abspath(os.path.dirname(__file__))  # get relative directory
 _datafolder = 'data'  # data directory
-_exampledata = 'example_data1.csv'  # data file
+_datafile = 'example_data1.csv'  # data file
 
 
 # MAIN FUNCTION
@@ -33,14 +33,19 @@ def read_file():
     data = []
 
     # auto close on return with 'with' function
-    with open(os.path.join(_basedir,
-                           _datafolder,
-                           _exampledata),
-              'r') as readhandle:
-        csvhandle = csv.reader(readhandle)  # use csv library reader function
+    try:
+        with open(os.path.join(_basedir,
+                               _datafolder,
+                               _datafile),
+                  'r') as readhandle:
+            csvhandle = csv.reader(readhandle)  # use csv library reader function
 
-        for row in csvhandle:  # append out of reader class
-            data.append(row)
+            for row in csvhandle:  # append out of reader class
+                data.append(row)
+    except FileNotFoundError:
+        print("WARNING NO FILE FOUND!")
+        print(usage)
+        sys.exit(0)
 
     return data
 
@@ -48,7 +53,7 @@ def read_file():
 def write_csv(corrected):
     with open(os.path.join(_basedir,
                            _datafolder,
-                           'correct.csv'),
+                           _correctfile),
               'w+') as writehandle:
         writer = csv.writer(writehandle)
         for row in corrected:
@@ -65,21 +70,21 @@ def spellcheck(data):
         if len(corrected) == 0:  # ignore header for correction
             corrected.append(i)
             continue
-        row = []
+        row = []  # used to recreate row for write
         for j in i:  # for column in row
-            column = []
+            column = []  # used to recreate column for write
             j = j.split()  # split string into list of words
             if len(j) > 1:  # if more than one word
                 for k in j:  # for word in sentence
                     # this needs to be better had to add len check
                     # because non alpha chars mess up first len check
                     if not spell.check(k) and len(k) > 1:
-                        column.append(spell.suggest(k)[0])
+                        column.append(spell.suggest(k)[0])  # add to column
                         continue
                     else:
-                        column.append(k)
+                        column.append(k)  # add to column
                 row.append(' '.join(column))
-            else:  # only one word so no loop needed
+            else:  # only one word so no loop needed also means new column
                 if not spell.check(j[0]):
                     row.append(spell.suggest(j[0])[0])
                     continue
@@ -91,4 +96,26 @@ def spellcheck(data):
 
 
 if __name__ == '__main__':
+
+    usage ="""
+            Usage: --\n 
+                spellchecker.py {input} {output}\n
+                running spellchecker.py with no args\n
+                returns in running test file example_data1.csv\n
+            """
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+            print(usage)
+            sys.exit(0)
+        _datafolder = ''
+        _datafile = sys.argv[1]
+
+        try:
+            _correctfile = sys.argv[2]
+        except IndexError:
+            _correctfile = 'correct.csv'
+            print("No output file specified outputting to: {}".format(_correctfile))
+
     main()  # run main function
+
